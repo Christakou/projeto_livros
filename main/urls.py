98b17c94030ab95a4c23aa93d7a7755a9b41a85b
Christakou <christiano.christakou@gmail.com>
2022-10-05 22:40:10 +0100

    adding sitemap

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml')),
     path("", views.HomePageView.as_view(), name="homepage"),
     path("pessoas/",views.PeopleView.as_view(),name="people"),
         path('<str:slug>/',views.PersonView.as_view(), name="ppl"),
