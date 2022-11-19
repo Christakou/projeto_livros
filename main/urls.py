b2e4582eecf65fa21c4a748553f6321040e7cb12
Christakou <christiano.christakou@gmail.com>
2022-11-19 20:06:15 +0000

    feat: added robots.txt and metatags

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt')),
     path("", views.HomePageView.as_view(), name="homepage"),
     path("pessoas/",views.PeopleView.as_view(),name="people"),
         path('livros-recomendados-por-<str:slug>/',views.PersonView.as_view(), name="ppl"),
