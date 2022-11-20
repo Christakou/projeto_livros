94b9618410bd275051f8b56590ba4f1e372fc4e8
Christakou <christiano.christakou@gmail.com>
2022-11-20 02:11:51 +0000

    feat: Adding rolling window

--- main/views.py
+++ main/views.py
@@ -3,28 +3,45 @@ from django.views import View
 from django.shortcuts import render, redirect
 from .models import Book, Influencer
 
+
 class HomePageView(TemplateView):
 
     def get(self, request, **kwargs):
-        filtered_list = ['Elon Musk','Bill Gates', 'Barack Obama']
+        filtered_list = [
+            'Elon Musk',
+            'Bill Gates', 
+            'Barack Obama', 
+            'Steve Jobs',
+            'Joe Rogan',
+            'Michael Bloomberg',
+            'Jeff Bezos',
+            'Taylor Swift',
+            'Khloe Kardashian'
+            'Bill Clinton',
+            'Kanye West',
+            'Mark Zuckerberg',
+            'Michele Obama',
+            'Keanu Reeves'
+            ]
+        
         featured_people = Influencer.objects.filter(name__in=filtered_list)
-        context = {"featured_influencers":featured_people}
+        print(featured_people)
+        context = {"featured_influencers": featured_people}
         return render(request, 'main/home.html', context=context)
 
 
 class PeopleView(View):
 
     def get(self, request, *args, **kwargs):
-        influencers = [item for item in Influencer.objects.all()]   
-        return render(request,'main/people.html',{'people': influencers})
-        
+        influencers = [item for item in Influencer.objects.all()]
+        return render(request, 'main/people.html', {'people': influencers})
 
-class PersonView(View): 
+
+class PersonView(View):
     def get(self, request, *args, **kwargs):
-        slug= kwargs.get('slug')
+        slug = kwargs.get('slug')
         influencer = Influencer.objects.filter(slug=slug).first()
         if influencer is None:
-            return(redirect('/'))
+            return (redirect('/'))
         else:
-            return render(request,'main/person.html',{'influencer':influencer})
-    
+            return render(request, 'main/person.html', {'influencer': influencer})
